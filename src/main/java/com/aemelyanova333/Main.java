3dc11a41a4fe9b5dff53080c05bf84c766e07295
Anastasia Emelyanova <aemelyanova333@gmail.com>
2023-03-29 20:16:27 +0300

    added getMessage() method

--- src/main/java/com/aemelyanova333/Main.java
+++ src/main/java/com/aemelyanova333/Main.java
@@ -2,6 +2,9 @@ package com.aemelyanova333;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getMessage());
+    }
+    private static String getMessage(){
+        return "Hello world!";
     }
 }
