2abf8bb6596b74e29250a587072ff44643c077a9
aemelyanova <123396410+aemelyanova@users.noreply.github.com>
2023-03-29 20:32:49 +0300

    Update Main.java

--- src/main/java/com/aemelyanova333/Main.java
+++ src/main/java/com/aemelyanova333/Main.java
@@ -2,7 +2,7 @@ package com.aemelyanova333;
 
 public class Main {
     
-    public static String message = "Hello world!!!!!!!!!!!!!"
+    public static String message = "Hello world!!"
         
     public static void main(String[] args) {
         System.out.println(message();
